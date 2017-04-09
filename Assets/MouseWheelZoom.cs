058f26f82ffa1e1cb6baa5a6dfed79175bf3961b
Benjamin <benjamin.guilloteau@gmail.com>
2017-04-09 22:31:58 +0200

    add camera movement on mouse wheel and click

---
+++ Assets/MouseWheelZoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWheelZoom : MonoBehaviour
+{
+
+    public float minSize;
+    public float maxSize;
+    public float sensitivity;
+    private Vector3 mousePosition;
+    public float moveSpeed;
+
+
+
+    void Update()
+    {
+        // try to move camera
+        if (Input.GetMouseButton(1))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10); // to set z to -10...
+        }
+
+        // zoom in/out on mouse wheel
+        var camera = this.GetComponent<Camera>();
+        float size = camera.orthographicSize;
+        size -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        size = Mathf.Clamp(size, minSize, maxSize);
+        camera.orthographicSize = size;
+
+
+
+    }
+
+}
