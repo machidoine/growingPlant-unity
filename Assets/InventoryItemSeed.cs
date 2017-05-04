d05db949ac7820041c568f6babbc30c626d58b56
Benjamin <benjamin.guilloteau@gmail.com>
2017-05-04 22:38:20 +0200

    add skill color under the seed picture in the inventory

--- Assets/InventoryItemSeed.cs
+++ Assets/InventoryItemSeed.cs
@@ -7,11 +7,14 @@ using UnityEngine.UI;
 
 public class InventoryItemSeed : InventoryItem
 {
-
-
     public ParentChangedEvent ParentChanged { get; set; }
     public Text IdText;
     private Seed _model;
+
+    public Transform skillPanel;
+    public SkillColor[] skillColors;
+    public SkillInventoryCounter skillFab;
+
     public float Id { set { IdText.text = value.ToString(); } }
 
 
@@ -26,6 +29,12 @@ public class InventoryItemSeed : InventoryItem
         {
             Id = value.id;
             _model = value;
+
+            addSkill(value.skills.attack, SkillTypeEnum.Attack);
+            addSkill(value.skills.defense, SkillTypeEnum.Defense);
+            addSkill(value.skills.fertility, SkillTypeEnum.Fertility);
+            addSkill(value.skills.growth, SkillTypeEnum.Growth);
+            addSkill(value.skills.victory, SkillTypeEnum.Victory);
         }
         get
         {
@@ -44,6 +53,34 @@ public class InventoryItemSeed : InventoryItem
         ParentChanged.Invoke(this);
     }
 
+    private void addSkill(int value, SkillTypeEnum skillType)
+    {
+        if (value != 0)
+        {
+            var skill = Instantiate(skillFab, skillPanel);
+            skill.Value = value;
+
+            var skillC = Array.Find(skillColors, skillColor => skillColor.skillType == skillType);
+            skill.Color = skillC.color;
+        }
+    }
+
     [Serializable]
     public class ParentChangedEvent : UnityEvent<InventoryItemSeed> { };
+}
+
+[Serializable]
+public class SkillColor
+{
+    public SkillTypeEnum skillType;
+    public Color color;
+}
+
+public enum SkillTypeEnum
+{
+    Attack,
+    Defense,
+    Growth,
+    Victory,
+    Fertility
 }
